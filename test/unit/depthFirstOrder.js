a7462107fd3141c855401e63df65606ebb51e542
Jim Bulkowski <jim.b@paperelectron.com>
2016-12-26 13:27:26 -0500

    Allows external config to add dependencies to plugin. Closes #5

---
+++ test/unit/depthFirstOrder.js
@@ -0,0 +1,185 @@
+/**
+ * @file naturalOrdering
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-loader
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+"use strict";
+
+var tap = require('tap');
+var mockery = require('mockery');
+var path = require('path');
+mockery.enable({
+  useCleanCache: true,
+  warnOnUnregistered: false
+});
+var DI = require('magnum-di');
+var injector = DI();
+var FrameworkInjector = DI();
+var NameGenerator = require(path.join(__dirname, '../../', 'lib/Validators/NameGenerator'))
+var Iterator = require('../../lib/PluginIterator');
+var Output = require('../../lib/Outputs');
+var RawPlugin = require('../../lib/RawPlugin/Types/Dependency');
+var Plugin = require('../../lib/Plugin/Plugin');
+
+
+var OptionValidators = require(path.join(__dirname, '../../', 'lib/Validators/FrameworkOptionValidators'));
+var mockSettingsPath = path.join(__dirname, '../mocks/_unit/depthFirstOrder/pluginSettings');
+var instanceObjects = {
+  ParentDirectory: path.join(__dirname, '../'),
+  Loggers: {
+    SystemLogger: mockConsole(),
+    FrameworkLogger: mockConsole(),
+    Output: Output(false, false)
+  },
+  FrameworkErrors: require('../../lib/Errors'),
+  Injector: injector,
+  FrameworkInjector: FrameworkInjector,
+  FrameworkOptions: {
+    prefix: 'magnum',
+    timeout: 2000,
+    layers: ['core'],
+    pluginSettingsDirectory: OptionValidators.findPluginSettings(mockSettingsPath)
+  }
+};
+
+FrameworkInjector.service('Options', instanceObjects.FrameworkOptions)
+FrameworkInjector.service('LoadedModuleNames', ['TestB','TestC','TestG','TestA','TestF','TestE','TestD'])
+FrameworkInjector.service('LoggerBuilder', function(){
+  return mockConsole()
+})
+
+FrameworkInjector.service('NameGenerator', NameGenerator('magnum'))
+
+var Shared = {
+  ParentDirectory: instanceObjects.ParentDirectory,
+  Logger: instanceObjects.Loggers.Logger,
+  Injector: instanceObjects.Injector,
+  FrameworkInjector: instanceObjects.FrameworkInjector,
+  Output: instanceObjects.Loggers.Output,
+  FrameworkErrors: instanceObjects.FrameworkErrors,
+  FrameworkOptions: instanceObjects.FrameworkOptions
+};
+
+var plugins = [
+  makePlugin('test-a'),
+  makePlugin('test-b'),
+  makePlugin('test-c'),
+  makePlugin('test-d', ['TestE']),
+  makePlugin('test-e', ['TestF']),
+  makePlugin('test-f', ['TestG']),
+  makePlugin('test-g')
+]
+var expectedOrder = FrameworkInjector.get('LoadedModuleNames')
+instanceObjects.loadedModuleNames = expectedOrder
+
+
+var iteratorInst;
+
+tap.test('Iterator instantiation', function(t) {
+  t.plan(1);
+  try {
+    iteratorInst = new Iterator(plugins, instanceObjects);
+  }
+  catch(e){
+    console.log(e.stack);
+  }
+  t.equal(iteratorInst instanceof Iterator, true, "Should create an Iterator instance.");
+});
+
+
+
+tap.test('Iterator load method, correct order, with external dependency constraint', function(t) {
+
+  iteratorInst.load()
+    .then(function(result) {
+      result.forEach(function(plugin, k){
+        console.log(plugin.configName);
+        t.same(plugin.configName, expectedOrder[k], plugin.configName + ' - Should match expected order ' + expectedOrder[k])
+        t.ok(plugin.loaded, plugin.declaredName + ' loaded.');
+      })
+      t.end()
+      return null
+    })
+  // .catch(function(err) {
+  //   console.log(iteratorInst);
+  // })
+
+});
+
+tap.test('Iterator start method, correct order', function(t) {
+  iteratorInst.start()
+    .then(function(result) {
+      result.forEach(function(plugin, k){
+        t.same(plugin.configName, expectedOrder[k], plugin.configName + ' - Should match expected order ' + expectedOrder[k])
+        t.ok(plugin.started, plugin.declaredName + ' started.');
+      })
+      t.end()
+      return null
+    })
+
+});
+
+tap.test('Iterator stop method, correct order', function(t) {
+
+  iteratorInst.stop()
+    .then(function(result) {
+      var reverseExpected = expectedOrder.reverse()
+      result.forEach(function(plugin, k){
+
+        t.same(plugin.configName, reverseExpected[k], plugin.configName + ' - Should match expected order ' + reverseExpected[k])
+        // t.ok(plugin.stopped, plugin.declaredName + ' stopped.');
+      })
+      t.end()
+      return null
+    })
+
+});
+
+
+
+
+function makePlugin(moduleName, depends, provides) {
+  var pArgs =
+        {
+          loaded: {
+            options: {name: moduleName},
+            metadata: {
+              name: moduleName,
+              layer: 'core',
+              type: 'service',
+              depends: depends || [],
+              provides: provides || [],
+              param: moduleName.replace('-', '_')},
+            plugin: {
+              load: function(inject, loaded) {
+                loaded(null, {name: moduleName})
+              },
+              start: function(done) {
+                done()
+              },
+              stop: function(done) {
+                done()
+              }
+            }
+          },
+          moduleName: moduleName
+        };
+  var plugin = new RawPlugin(pArgs, instanceObjects.FrameworkOptions.layers)
+  return new Plugin(plugin, Shared)
+}
+
+function mockConsole() {
+  // return console
+  return {
+    log: function() {
+    },
+    error: function() {
+    },
+    info: function() {
+    },
+    warn: function() {
+    },
+  }
+}
