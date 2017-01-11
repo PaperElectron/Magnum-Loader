0f20a19e7fffdc66eea40708d5e0f3a13c170b0b
Jim Bulkowski <jim.b@paperelectron.com>
2017-01-11 15:10:04 -0500

    Wipes /test

--- test/mocks/_integration/pluginConfiguration/pluginSettings/ConfigDisabled.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file ConfigDisabled
- * @author Jim Bulkowski <jim.b@paperelectron.com>
- * @project Pomegranate-loader
- * @license MIT {@link http://opensource.org/licenses/MIT}
- */
-module.exports.ConfigDisabled = {
-  disabled: true
-};
--- test/mocks/_unit/depthFirstOrder/pluginSettings/TestA.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by monstertke on 12/26/16.
- */
-
-exports.TestA = function(Env){
-  return {
-    additionalDependencies: ['TestB']
-  }
-}
